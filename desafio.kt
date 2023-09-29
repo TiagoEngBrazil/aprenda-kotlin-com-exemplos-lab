ad82d80d9560c15545b45e39099c5290001b341f
Tiago Lima Pimenta <102493965+TiagoEngBrazil@users.noreply.github.com>
2023-09-28 21:57:56 -0300

    Update desafio.kt
    Meu código para o desafio!

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,56 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(val nome: String, val email: String, val telefone: Long)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+class InformacaoEducacional(val nomeFormacao: String, val tempoDias: Int, val nivel: Nivel)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nomeFormacao: String, val nivel: Nivel, val conteudos: MutableList<InformacaoEducacional> = mutableListOf()) {
+    private val inscritos = mutableListOf<Usuario>()
 
-    val inscritos = mutableListOf<Usuario>()
-    
+    companion object {
+        private val inscritosPorFormacaoENivel = mutableMapOf<Pair<String, Nivel>, Int>()
+
+        fun getQuantidadeInscritos(formacao: Formacao): Int {
+            val key = Pair(formacao.nomeFormacao, formacao.nivel)
+            return inscritosPorFormacaoENivel[key] ?: 0
+        }
+
+        fun incrementarQuantidadeInscritos(formacao: Formacao) {
+            val key = Pair(formacao.nomeFormacao, formacao.nivel)
+            val quantidadeAtual = inscritosPorFormacaoENivel[key] ?: 0
+            inscritosPorFormacaoENivel[key] = quantidadeAtual + 1
+        }
+    }
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+        incrementarQuantidadeInscritos(this)
+    }
+
+    fun adicionarConteudo(conteudo: InformacaoEducacional) {
+        conteudos.add(conteudo)
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val matricula1 = Formacao("JavaScript/HTML/CSS", Nivel.INTERMEDIARIO)
+    val matricula2 = Formacao("JavaScript/HTML/CSS", Nivel.INTERMEDIARIO)
+    val matricula3 = Formacao("WordPress", Nivel.DIFICIL)
+
+    val aluno1 = Usuario("Paula", "pLima@gmail.com", 87992719933)
+    val aluno2 = Usuario("João", "joao@gmail.com", 5599271453)
+    val aluno3 = Usuario("Lucas", "lucasB@gmail.com", 4599271453)
+
+    val conteudoAluno1 = InformacaoEducacional(matricula1.nomeFormacao, 600, Nivel.BASICO)
+    val conteudoAluno2 = InformacaoEducacional(matricula1.nomeFormacao, 40, Nivel.INTERMEDIARIO)
+
+    matricula1.adicionarConteudo(conteudoAluno1)
+    matricula2.adicionarConteudo(conteudoAluno2)
+    matricula3.adicionarConteudo(conteudoAluno2)
+    matricula1.matricular(aluno1)
+    matricula2.matricular(aluno2)
+    matricula3.matricular(aluno3)
+
+    println("Quantidade de inscritos na formação ${matricula1.nomeFormacao}, nível ${matricula1.nivel}: ${Formacao.getQuantidadeInscritos(matricula1)}")
+    println("Quantidade de inscritos na formação ${matricula3.nomeFormacao}, nível ${matricula3.nivel}: ${Formacao.getQuantidadeInscritos(matricula3)}")
 }
